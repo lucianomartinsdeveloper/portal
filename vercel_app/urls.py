c1cc5ef7d0ba81b8b1fb19cd53a7cc6f5b717a7a
Luciano da Silva Martins <zicadopv@gmail.com>
2023-07-03 19:04:24 -0300

    Add urls in the folder vercel_app

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example.urls')),
+    # path('', include('example.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("djoser.urls")),
+    path("api/v1/", include("djoser.urls.authtoken")),
 ]
